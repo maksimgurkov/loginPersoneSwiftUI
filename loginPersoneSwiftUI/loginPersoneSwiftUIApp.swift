687c4b3539b5ce241b3e29c6de766968e2fe20b8
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-08 17:57:54 +0300

    Initial Commit

---
+++ loginPersoneSwiftUI/loginPersoneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginPersoneSwiftUIApp.swift
+//  loginPersoneSwiftUI
+//
+//  Created by Максим Гурков on 08.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct loginPersoneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
